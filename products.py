dde6cf8bc8014cead42992d8a075e3c01aff128f
NakamuraYUKI <tsugawa2580kouki@outlook.jp>
2026-07-04 08:54:06 +0000

    add

---
+++ products.py
@@ -0,0 +1,26 @@
+products = {
+    "Joint": [
+        {
+            "name": "やる気のないきのこ",
+            "price": 50000,
+            "increments": [1, 5, 10]
+        }
+    ],
+    "Other": [
+        {
+            "name": "まつたけ",
+            "price": 80000,
+            "increments": [1, 5, 10]
+        },
+        {
+            "name": "まいたけ",
+            "price": 80000,
+            "increments": [1, 5, 10]
+        },
+        {
+            "name": "えのきだけ",
+            "price": 80000,
+            "increments": [1, 5, 10]
+        }
+    ]
+}
