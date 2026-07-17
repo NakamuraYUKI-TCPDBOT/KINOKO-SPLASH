8ea4cf20ccba4e84383893a21e8aaf591fe4c587
NakamuraYUKI <tsugawa2580kouki@outlook.jp>
2026-07-18 04:33:07 +0900

    Update products.py

--- products.py
+++ products.py
@@ -22,5 +22,21 @@ products = {
             "price": 80000,
             "increments": [1, 5, 10]
         }
-    ]
+    ],
+    "夏限定":[
+        {
+            "name": "海辺のきのこ",
+            "price": 50000,
+            "increments": [1, 5, 10]
+        },
+        {
+            "name": "やばめなきのこ",
+            "price": 80000,
+            "increments": [1, 5, 10]
+        },
+        {
+            "name": "夏祭りきのこ",
+            "price": 100000,
+            "increments": [1, 5, 10]
+        }
 }
